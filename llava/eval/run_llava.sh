f4cf47c366013fcff0d4303ef120f9be225c1167
Ethan07902050 <r11922163@csie.ntu.edu.tw>
2024-01-11 16:04:05 +0800

    generate pororo background prompt

---
+++ llava/eval/run_llava.sh
@@ -0,0 +1,5 @@
+python run_llava_pororo.py \
+    --model-path liuhaotian/llava-v1.5-7b \
+    --query "Describe the background of the image." \
+    --mode train \
+    --output-path /work/u1509343/storydalle/data/pororo_png/background_prompt_train.json
